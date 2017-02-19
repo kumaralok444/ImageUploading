e71bdff5a384c204ed58c17844ef1551f06cd269
Alok Kumar <kumaralok444@gmail.com>
2017-02-19 16:43:57 +0530

    Javascript forloop update
    loop with condition

---
+++ AngularJS/js/Angular.js
@@ -0,0 +1,15 @@
+/**
+ * Created by NEW on 19-02-2017.
+ */
+(function () {
+    var app=angular.module("mymodule",[]);
+    app.controller("Mycontroller",function () {
+        this.info=obj;
+    });
+var obj=[{img:"images/Lunch.jpeg",name:"Lunch Boxes",price:"299-699",available:true},
+    {img:"images/Door.jpeg",name:"Door & Window",price:"149-399",available:true},
+    {img:"images/Decor.jpeg",name:"Home Decor Range",price:"Extra 30% Off",available:true},
+    {img:"images/Decor.jpeg",name:"Home Decor Range",price:"Extra 30% Off",available:true},
+    {img:"images/Decor.jpeg",name:"Home Decor Range",price:"Extra 30% Off",available:true}
+];
+})();
